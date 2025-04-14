ef971f223f163c2352b3a3a16a8a972609fa26e2
Raoul van Hezel <r.vanhezel@gmail.com>
2025-04-14 15:42:19 +0200

    try catch for system init

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from src.utilities.logger import Logger
 from src.configuration import Configuration
 from dotenv import load_dotenv
 from src.trading_system import TradingSystem
+import logging
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    Logger()
-    cfg = Configuration('run.cfg')
+    try:
 
-    trading_system = TradingSystem(cfg)
-    trading_system.start()
+        Logger()
+        cfg = Configuration('run.cfg')
+
+        trading_system = TradingSystem(cfg)
+        trading_system.start()
+
+    except Exception as e:
+        logging.error(f"Trading system initialization failed: {e}")
