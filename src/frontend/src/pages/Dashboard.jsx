70af2c0b9ac4f32f6e639acfd29df3b2ebc380a2
Raoul van Hezel <r.vanhezel@gmail.com>
2025-04-19 16:20:58 +0200

    updated js getstatus error message

--- src/frontend/src/pages/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard.jsx
@@ -37,7 +37,7 @@ const Dashboard = ({ systemError, setSystemError, systemSuccess, setSystemSucces
 
     } catch (err) {
       console.log('Error fetching status', err.message);
-      setError(err.message);
+      setError(`Error fetching status: ${err.message}`);
 
     } finally {
       setIsLoading(false);
